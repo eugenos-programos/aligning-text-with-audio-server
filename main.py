cc88719f4ff249dfe1a02fc4160be0f2fba1fca0
eugenos-programos <elena.samchuk.73@mail.ru>
2024-01-12 10:49:17 +0300

    remove unused files

--- main.py
+++
@@ -1,9 +0,0 @@
-from generate_timelines import generate_timelines
-import whisper_timestamped as whisper 
-
-
-if __name__ == '__main__':
-    file_name = ""
-    audio = whisper.load_audio(file_name)
-    model = whisper.load_model("tiny", device='cpu')
-    generate_timelines()
